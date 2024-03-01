a72f716a1507b08cba4ad2ca5251887ea2568eae
Nurislam Abdimalikov <98372q@gmail.com>
2024-03-01 17:26:34 +0600

    hw-21

---
+++ hw-7/script.js
@@ -0,0 +1,65 @@
+// zadacha 1
+
+// let button = document.querySelector('#js-button')
+
+// let showClass = () => {
+//     if (button.classList.contains('js-button')) {
+//         return "Данный класс имеется"
+//     } else {
+//         return " Данный класс не имеется"
+//     }
+// }
+// button.onclick = function () {
+//     console.log(showClass())
+//     alert(showClass())
+// }
+
+
+// Zadacha 2
+
+// const btn = document.querySelector(".js-button")
+
+// function changeButtonText() {
+//     if(btn.innerText === "On"){
+//         btn.innerHTML = 'Off'
+//         btn.classList.add("js-button-red")
+//     }else {
+//         btn.innerHTML = "On"
+//         btn.classList.remove("js-button-red")
+//     }
+// }
+
+// zadacha 3
+
+// function submitForm() {
+
+//     let inputValue = document.querySelector(".js-input").value;
+
+//     let result = document.getElementById("result");
+//     result.innerHTML = "<p>Your Name is: " + inputValue + "</p>";
+// }
+
+// zadacha 4
+
+// document.querySelector(".js-input").addEventListener("keyup", function(event) {
+//     if (event.key === "Enter") {
+//         submitForm();
+//     }
+// });
+
+// zadacha 5
+
+// function submitForm() {
+
+//     let inputValue = document.querySelector(".js-input").value;
+
+//     let result = document.getElementById("result");
+//     result.innerHTML = "<p>Your Name is: " + inputValue + "</p>";
+// }
+
+
+// document.querySelector(".js-input").addEventListener("input", function() {
+
+//     submitForm();
+// });
+
