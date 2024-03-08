63079b9a1ecab0689867f561fd79f07adaf719a0
Nurislam Abdimalikov <98372q@gmail.com>
2024-03-08 16:20:33 +0600

    hw-23

---
+++ hw-9/script.js
@@ -0,0 +1,98 @@
+//! zadacha1
+
+// const user = {
+//     id: 1,
+//     first_name: "Garrott",
+//     last_name: "Eberdt",
+//     email: "geberdt0@wp.com",
+//     gender: "Male",
+//     ip_address: "49.230.15.110",
+//     isAdmin: true,
+
+// };
+
+// const { id, first_name, last_name, email, gender, ip_address, isAdmin } = user
+
+// console.log("ID:", id);
+// console.log("Имя:", first_name);
+// console.log("Фамилия:", last_name);
+// console.log("Email:", email);
+// console.log("Пол:", gender);
+// console.log("IP-адрес:", ip_address);
+// console.log("isAdmin:", isAdmin);
+
+// ! zadacha 2
+
+// const animals = ["apple", "banana", "lemon", "kiwi",true];
+
+// let [alma,banan,lemons,kiwi,trues] = animals
+
+// console.log(alma);
+// console.log(banan);
+// console.log(lemons);
+// console.log(kiwi);
+// console.log(trues);
+
+// ! dop 
+
+// const workers = [
+//     {
+//       id: 1,
+//       name: "Emile",
+//       lastname: "Van Weedenburg",
+//       email: "evanweedenburg0@technorati.com",
+//     },
+//     {
+//       id: 2,
+//       name: "Orv",
+//       lastname: "Gretton",
+//       email: "ogretton1@about.me",
+//     },
+//   ];
+  
+
+//   for (const { id, name, lastname, email } of workers) {
+
+//     console.log("ID:", id);
+//     console.log("Имя:", name);
+//     console.log("Фамилия:", lastname);
+//     console.log("Email:", email);
+//     console.log("--------------");
+//   }
+  
+
+// ! zadacha 3
+
+// const add = () => {
+//     console.log(2+3);
+// }
+
+// add()
+
+// ! dop
+
+// const runTwice = (fun) => {
+//     fun()
+// }
+
+// runTwice(() => console.log('12b'));
+// runTwice(() => console.log(2+3));
+
+
+// ! zadacha 4
+
+// function startButton() {
+//     setTimeout(function() {
+//         document.querySelector("#buttons").innerText = 'Finished'
+//     }, 2000)
+// }
+
+// ! zadacha 5
+
+// function  startButtons() {
+//     document.querySelector("#buttonss").innerText = 'Loading...'
+
+//     setTimeout(function() {
+//         document.querySelector("#buttonss").innerText = 'Finished'
+//     }, 2000) 
+// }
